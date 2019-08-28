f21c8e6e0d910a75ad6295c63b5273e417b23429
tomotomo <tomotomobile@gmail.com>
2019-08-28 14:49:46 +0900

    config for GA

--- isaax-iotskill-function.php
+++ isaax-iotskill-function.php
@@ -12,7 +12,7 @@
  * ログイン時のリダイレクト先をトップページへ
  */
 add_action( 'admin_init', function () {
-	if( !current_user_can('administrator') ){
+	if( !current_user_can('edit_posts') ){
 		$home_url = site_url('', 'http');
 		wp_safe_redirect($home_url);
 		exit();
@@ -29,11 +29,23 @@ add_action( 'wp_logout', function () {
 });
 
 /**
- * 購読者がログイン時に管理バーを表示させない
+ * 投稿権限がないユーザーにはログイン時に管理バーを表示させない
  */
 add_filter( 'show_admin_bar', function ($content) {
-	if( current_user_can("administrator") ){
+	if( current_user_can("edit_posts") ){
 		return $content;
 	}
 	return false;
+});
+
+/**
+ * GAにUser-IDを送信する
+ */
+add_action( 'wp_footer', function () {
+	$user = wp_get_current_user();
+	if( in_array('subscriber', $user->roles)){
+		// ログインしている user_id を使用してUser-ID を設定します。
+		$script = "<script>gtag('set', {'user_id': '%s'});</script>";
+		echo sprintf($script, esc_attr($user->user_login));
+	}
 });
